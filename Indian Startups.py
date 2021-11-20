de7dead943f998f432109b7f3716b853b061d9a4
chpallavi222 <57914419+chpallavi222@users.noreply.github.com>
2021-11-20 13:22:04 +0530

    Add files via upload

---
+++ Indian Startups.py
@@ -0,0 +1,370 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# for mathematical operations
+import numpy as np 
+
+# for dataframe operations
+import pandas as pd 
+
+# for data visualizations
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# lets select  a background style
+plt.style.use('fivethirtyeight')
+plt.rcParams['figure.figsize'] = (16, 7)
+
+# for interactivity
+import ipywidgets as widgets
+from ipywidgets import interact
+from ipywidgets import interact_manual
+
+
+# In[2]:
+
+
+# lets import the data
+data = pd.read_csv("D:/Indian Startups/EDA on Finance Data/startup_funding.csv")
+
+# changing the names of the columns inside the data
+data.columns = ["SNo", "Date", "StartupName", "IndustryVertical", "SubVertical",
+                        "City", "InvestorsName", "InvestmentType", "AmountInUSD", "Remarks"]
+
+# lets clean the strings
+def clean_string(x):
+    return str(x).replace("\\xc2\\xa0","").replace("\\\\xc2\\\\xa0", "")
+
+# lets apply the function to clean the data
+for col in ["StartupName", "IndustryVertical", "SubVertical", "City", 
+            "InvestorsName", "InvestmentType", "AmountInUSD", "Remarks"]:
+    data[col] = data[col].apply(lambda x: clean_string(x))
+    
+# lets check the shape of the data
+print("Size of data", data.shape)
+
+# lets check the column names of the data
+print(data.columns)
+
+
+# In[3]:
+
+
+# missing data 
+
+# lets import warnings module
+import warnings
+warnings.filterwarnings('ignore')
+
+# lets calculate the total missing values in the data
+total = data.isnull().sum().sort_values(ascending = False)
+
+# lets calculate the percentage of missing values in the data
+percent = ((data.isnull().sum()/data.isnull().count())*100).sort_values(ascending = False)
+
+# lets store the above two values in a dataset called missing data
+missing_data = pd.concat([total, percent], axis=1, keys=['Total', 'Percent %'])
+
+# lets check the head of the data
+missing_data
+
+
+# In[4]:
+
+
+# lets check the values in the Remarks column
+data['Remarks'].value_counts()
+
+
+# In[5]:
+
+
+# lets remove Remarks column, as it contains a lot of nans, and high cardinal column
+
+data = data.drop(['Remarks'], axis = 1)
+
+# lets check the column names after removing the Remarks Column, as it having 
+data.columns
+
+
+# In[6]:
+
+
+# lets convert the amount column into numerical, so that we can analyze the values inside it
+
+# function to clean the AmounInUsd Column
+def clean_amount(x):
+    x = ''.join([c for c in str(x) if c in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']])
+    x = str(x).replace(",","").replace("+","")
+    x = str(x).lower().replace("undisclosed","")
+    x = str(x).lower().replace("n/a","")
+    if x == '':
+        x = '-999'
+    return x
+
+# lets apply the function on the column
+data["AmountInUSD"] = data["AmountInUSD"].apply(lambda x: float(clean_amount(x)))
+
+# lets check the head of the column after cleaning it
+plt.rcParams['figure.figsize'] = (15, 3)
+data['AmountInUSD'].plot(kind = 'line', color = 'black')
+plt.title('Distribution of Amount', fontsize = 15)
+plt.show()
+
+
+# In[7]:
+
+
+data['Date'][data['Date']=='12/05.2015'] = '12/05/2015'
+data['Date'][data['Date']=='13/04.2015'] = '13/04/2015'
+data['Date'][data['Date']=='15/01.2015'] = '15/01/2015'
+data['Date'][data['Date']=='22/01//2015'] = '22/01/2015'
+data['Date'][data['Date']=='05/072018'] = '05/07/2018'
+data['Date'][data['Date']=='01/07/015'] = '01/07/2015'
+data['Date'][data['Date']=='\\\\xc2\\\\xa010/7/2015'] = '10/07/2015'
+
+
+# ### How Does the Funding Ecosystem changes with respect to Time?
+
+# In[8]:
+
+
+# some of the date formats are written wrongly so we have to clean them thoroughly so that we can analyze these values
+
+# converting them into a Datetime object
+data["yearmonth"] = (pd.to_datetime(data['Date'],
+         format='%d/%m/%Y').dt.year*100)+(pd.to_datetime(data['Date'],format='%d/%m/%Y').dt.month)
+
+
+temp = data['yearmonth'].value_counts().sort_values(ascending = False).head(10)
+print("Number of funding per month in decreasing order(Top 10)\n",temp)
+year_month = data['yearmonth'].value_counts()
+
+# lets plot the data
+plt.rcParams['figure.figsize'] = (15, 7)
+sns.barplot(year_month.index, year_month.values, palette = 'copper')
+plt.xticks(rotation = 90)
+plt.xlabel('Year-Month of transaction', fontsize=12)
+plt.ylabel('Number of fundings made', fontsize=12)
+plt.title("Year-Month Distribution", fontsize=16)
+plt.show()
+
+
+# #### What is the General Amount that Startups get in India?
+
+# In[9]:
+
+
+# lets check the Maximum funding of a startup
+print("Maximum funding to a Startups is : ", data["AmountInUSD"].dropna().sort_values().max())
+
+
+# In[10]:
+
+
+def check(x = 500000000): # 50 crore funding of startups
+    return data[data['AmountInUSD'] > x][['StartupName',
+                                   'IndustryVertical',
+                                   'AmountInUSD']].sort_values(by = 'AmountInUSD', 
+                                                               ascending = False)
+check()
+
+
+# In[11]:
+
+
+# lets check out different ventures of Paytm
+data[data.StartupName == 'Paytm'][['StartupName','InvestmentType']]
+
+
+# In[12]:
+
+
+# lets check out different ventures of Flipkart
+data[data.StartupName == 'Flipkart'][['StartupName','InvestmentType']]
+
+
+# In[13]:
+
+
+# lets check the minimum funding in a startup
+print("Minimum funding to a Startups is : ", data["AmountInUSD"].dropna().sort_values().min())
+
+
+# In[14]:
+
+
+# lets check the startups with least funding
+data[['AmountInUSD','StartupName']].sort_values(by = 'AmountInUSD', ascending = True).head(5)
+
+
+# In[15]:
+
+
+data[data['AmountInUSD'] == -999.0].shape
+
+
+# In[16]:
+
+
+# Average Funding
+print("On Average indian startups got funding of : ", data["AmountInUSD"].dropna().sort_values().mean())
+
+
+# In[17]:
+
+
+# lets check the no. of funding each startsup got
+
+print("Total startups funded : ", len(data["StartupName"].unique()))
+print(data["StartupName"].value_counts().head(10))
+startupname = data['StartupName'].value_counts().head(20)
+
+# lets plot the data
+plt.rcParams['figure.figsize'] = (12, 15)
+sns.barplot(y = startupname.index, x = startupname.values, alpha=0.9, palette = 'Dark2')
+plt.xticks(rotation='vertical')
+plt.xlabel('Startup Name', fontsize=12)
+plt.ylabel('Number of fundings made', fontsize=12)
+plt.title("Number of fundings Startups Have", fontsize=16)
+plt.show()
+
+
+# #### Which Kind of Industries are more preferred for Startups?
+
+# In[18]:
+
+
+# lets analyze the Industry of the Startups
+
+# data cleaning
+data['IndustryVertical'] = data['IndustryVertical'].replace('nan', 'Consumer Technology')
+
+industry = data['IndustryVertical'].value_counts().head(10)
+print(industry)
+
+# lets plot the data
+plt.rcParams['figure.figsize'] = (15, 5)
+sns.barplot(industry.index, industry.values, palette = 'autumn')
+plt.xticks(rotation='vertical')
+plt.xlabel('Industry vertical of startups', fontsize=12)
+plt.ylabel('Number of fundings made', fontsize=12)
+plt.title("Industry vertical of startups with number of funding", fontsize=16)
+plt.show()
+
+
+# In[19]:
+
+
+# lets analyze the sub vertical industries
+
+industry = data['SubVertical'].value_counts()[1:].head(20)
+print(industry)
+
+# lets plot the data
+sns.lineplot(industry.index, industry.values, palette = 'winter')
+plt.xticks(rotation='vertical')
+plt.xlabel('Subvertical of startups', fontsize=12)
+plt.ylabel('Number of fundings made', fontsize=12)
+plt.title("Subvertical of startups with number of funding", fontsize=16)
+plt.show()
+
+
+# #### Does Location also play a role, In determining the Growth of a Startup?
+
+# In[20]:
+
+
+# analyzing the effect of Cities on a Startup
+
+# lets clean the data for better analysis
+data['City'] = data['City'].replace(('Bengaluru', 'nan'),('Bangalore', 'Bangalore'))
+
+city = data['City'].value_counts().head(10)
+print(city)
+
+# lets plot the data
+sns.barplot(city.index, city.values, palette = 'Wistia')
+plt.xticks(rotation='vertical')
+plt.xlabel('city location of startups', fontsize=12)
+plt.ylabel('Number of fundings made', fontsize=12)
+plt.title("city location of startups with number of funding", fontsize=16)
+plt.show()
+
+
+# #### Who plays the main role in Indian Startups Ecosystem?
+
+# In[21]:
+
+
+from wordcloud import WordCloud
+
+names = data["InvestorsName"][~pd.isnull(data["InvestorsName"])]
+wordcloud = WordCloud(max_font_size=50, width=600, height=300, background_color = 'cyan').generate(' '.join(names))
+plt.figure(figsize=(15,8))
+plt.imshow(wordcloud)
+plt.title("Wordcloud for Investor Names", fontsize=35)
+plt.axis("off")
+plt.show()
+
+
+# In[22]:
+
+
+# lets analyze the investors on startups
+
+# lets clean the dataset
+data['InvestorsName'][data['InvestorsName'] == 'Undisclosed investors'] = 'Undisclosed Investors'
+data['InvestorsName'][data['InvestorsName'] == 'undisclosed Investors'] = 'Undisclosed Investors'
+data['InvestorsName'][data['InvestorsName'] == 'undisclosed investors'] = 'Undisclosed Investors'
+data['InvestorsName'][data['InvestorsName'] == 'Undisclosed investor'] = 'Undisclosed Investors'
+data['InvestorsName'][data['InvestorsName'] == 'Undisclosed Investor'] = 'Undisclosed Investors'
+data['InvestorsName'][data['InvestorsName'] == 'Undisclosed'] = 'Undisclosed Investors'
+data['InvestorsName'][data['InvestorsName'] == 'nan'] = 'Undisclosed Investors'
+data['InvestorsName'][data['InvestorsName'] == 'nan '] = 'Undisclosed Investors'
+
+
+# lets check the value counts
+investors = data['InvestorsName'].value_counts().head(10)
+print(investors)
+
+# lets plot the data
+sns.barplot(investors.index, investors.values, palette = 'cool')
+plt.xticks(rotation='vertical')
+plt.xlabel('Investors Names', fontsize=12)
+plt.ylabel('Number of fundings made', fontsize=12)
+plt.title("Investors Names with number of funding", fontsize=16)
+plt.show()
+
+
+# #### What are the different Types of Funding for Startups?
+
+# In[23]:
+
+
+# lets analyze the investment
+investment = data['InvestmentType'].value_counts().head(10)
+print(investment)
+
+# lets clean the dataset
+data['InvestmentType'][data['InvestmentType'] == 'SeedFunding'] = 'Seed Funding'
+data['InvestmentType'][data['InvestmentType'] == 'Seed\\nFunding'] = 'Seed Funding'
+data['InvestmentType'][data['InvestmentType'] == 'Crowd funding'] = 'Crowd Funding'
+data['InvestmentType'][data['InvestmentType'] == 'PrivateEquity'] = 'Private Equity'
+data['InvestmentType'][data['InvestmentType'] == 'Seed/ Angel Funding'] = 'Seed Angel Funding'
+data['InvestmentType'][data['InvestmentType'] == 'Seed/Angel Funding'] = 'Seed Angel Funding'
+data['InvestmentType'][data['InvestmentType'] == 'Seed /Angel Funding'] = 'Seed Angel Funding'
+data['InvestmentType'][data['InvestmentType'] == 'Seed / Angel Funding'] = 'Seed Angel Funding'
+data['InvestmentType'][data['InvestmentType'] == 'Angel / Seed Funding'] = 'Seed Angel Funding'
+
+# lets plot the data
+sns.barplot(investment.index, investment.values, palette = 'summer')
+plt.xticks(rotation='vertical')
+plt.xlabel('Investment Type', fontsize=12)
+plt.ylabel('Number of fundings made', fontsize=12)
+plt.title("Investment Type with number of funding", fontsize=16)
+plt.show()
+
